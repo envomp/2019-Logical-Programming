67de99552101846ca1a0afdff681b0e0f827a755
kirillsun <kideni@ttu.ee>
2019-09-01 23:26:03 +0300

    t01

--- t01_string_operations/t01_fstring_tests.py
+++ t01_string_operations/t01_fstring_tests.py
@@ -12,11 +12,22 @@ def mock_input_and_import_module(monkeypatch, value="a\nb\nc"):
 
 
 @pytest.mark.timeout(1.0)
-def test__greeting_sentence_is_correct(monkeypatch):
+def test__self_description_sentence_is_correct(monkeypatch):
     try:
         m = mock_input_and_import_module(monkeypatch)
         assert is_not_blank(m.first_name)
         assert is_not_blank(m.last_name)
         assert m.self_description_sentence == f'My name is {m.first_name} {m.last_name}'
+    except AttributeError:
+        pytest.fail('Variable not found!')
+
+
+@pytest.mark.timeout(1.0)
+def test__name_is_correct(monkeypatch):
+    try:
+        m = mock_input_and_import_module(monkeypatch)
+        assert is_not_blank(m.first_name)
+        assert is_not_blank(m.last_name)
+        assert m.name == f'{m.first_name} {m.last_name}'
     except AttributeError:
         pytest.fail('Variable not found!')
