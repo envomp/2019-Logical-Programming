ac961ed6c4fad786f55160557dd20ebac4f1f53b
envomp <envomp@ttu.ee>
2019-09-10 11:32:36 +0300

    PR01

---
+++ PR02/prax02.pl
@@ -0,0 +1,70 @@
+% kommentaar
+/*
+["Ago", "Peeter", "Mai", [peep, []], 89] list
+[El1, El2 |_]
+[_|Tail] <- eeldatalse vähemalt ühte elementi
+[Tail] <- cõib ka 0 olla
+
+predikaat/n
+
+, -ja
+; - või
+not - eitus
+-> - implikatsioon
+
+repeat - kutsub uuesti
+! -kärpimine, reeglist ära lõigata mingi usa
+fail - kustutab vahetulemuse ja hüppab algusesse
+
+consult - saab faili laadida
+reconsult- taaskasutada sama fail
+get - lugeda sümbol
+put - väljastab sümboli
+write - väljastab sõne
+
+div - jagamine
+max - maksimum mitmest elemendist
+min - minimum mitmest elemendist
+
+term_variables(+Term, -List).
+string_to_atom(?String, ?Atom).
+string_to_list(?String, ?List).
+string_to_length(+String, -Length).
+string_concat(?String1, ?String2, ?String3).
+sub_stirng(+String, ?Start, ?Length, ?After, ?Substring).
+
+findall(+Template, +Goal, -Bag).
+bagof(+Template, +Goal, -Bag).
+
+foo(a, b, c).
+foo(a, b, d).
+foo(b, c, e).
+foo(b, c, f).
+foo(c, c, g).
+
+bagof(C, foo(A, B, C), Cs).
+A = a, B = b, C=G308, Cs = [c, d] ;
+A = b, B = c, C=G308, Cs = [e, f] ;
+A = c, B = c, C=G308, Cs = [g]
+
+Dünaamilised muutujad.
+assert(Clause). - Loob predikaadi.
+asserta(Clause). - Lisatakse mälus ette. (Kohe kätte saadav)
+assertz(Clause). - Lisiatakse mälus järgi. (Viimane kätte saadav)
+
+retract(Clause). - esimene match kustutatakse
+retractall(Clause). - kõik kustutatakse
+abolish()
+
+Term =.. List
+
+foo(hello, X) =.. List.
+List = [foo, hello, X]
+
+Term =.. [baz, foo(1)].
+Term = baz(foo(1))
+
+*/
+
+min_of_3(A, B, C, D):- D = min(min(A, B), C).
+
