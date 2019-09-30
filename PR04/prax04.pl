d0c017abdee91fe63b156e622da83695ec9d25c7
envomp@ttu.ee <envomp@ttu.ee>
2019-09-30 21:09:31 +0300

    PR04, Scuse me WTF?

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -2,23 +2,28 @@ laevaga(tallinn, helsinki, 120).
 laevaga(tallinn, stockholm, 480).
 bussiga(tallinn, riia, 300).
 rongiga(riia, berlin, 680).
+rongiga(paris, japan, 120).
 lennukiga(tallinn, helsinki, 30).
 lennukiga(helsinki, paris, 180).
 lennukiga(paris, berlin, 120).
 lennukiga(paris, tallinn, 120).
 
 
-mineVahend(Start, End, lennukiga) :- lennukiga(Start, End, _).
-mineVahend(Start, End, rongiga) :- rongiga(Start, End, _).
-mineVahend(Start, End, bussiga) :- bussiga(Start, End, _).
-mineVahend(Start, End, laevaga) :- laevaga(Start, End, _).
+mineVahend(Start, End, lennukiga) :- lennukiga(Start, End, _); lennukiga(End, Start, _).
+mineVahend(Start, End, rongiga) :- rongiga(Start, End, _); rongiga(End, Start, _).
+mineVahend(Start, End, bussiga) :- bussiga(Start, End, _); bussiga(End, Start, _).
+mineVahend(Start, End, laevaga) :- laevaga(Start, End, _); laevaga(End, Start, _).
 
 
 mineHind(Start, End, Price) :-
     lennukiga(Start, End, Price);
     rongiga(Start, End, Price);
     bussiga(Start, End, Price);
-    laevaga(Start, End, Price).
+    laevaga(Start, End, Price);
+    lennukiga(End, Start, Price);
+    rongiga(End, Start, Price);
+    bussiga(End, Start, Price);
+    laevaga(End, Start, Price).
 
 
 reisi(Start, End) :-
@@ -56,7 +61,8 @@ reisi_transpordiga(Start, End, Road) :-
 
 
 path3(Start, Stop, _, mine(Start, Stop, Transport), Hind) :- mineVahend(Start, Stop, Transport),  mineHind(Start, Stop, Hind).
-path3(Start, Finish, Visited, mine(Start, Next), +(Trip, Price)) :-
+path3(Start, Finish, Visited, mine(Start, Stop, Transport, Next), +(Trip, Price)) :-
+    mineVahend(Start, Stop, Transport),
     mineHind(Start, Stop, Trip),
     not(member(Stop, Visited)),
     path3(Stop, Finish, [Stop | Visited], Next, Price).
