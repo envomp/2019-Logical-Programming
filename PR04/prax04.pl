06a24a4d8a868d83cf4098475c5a33d971a2fe2d
envomp@ttu.ee <envomp@ttu.ee>
2019-09-30 19:50:48 +0300

    PR04

--- PR04/prax04.pl
+++ PR04/prax04.pl
@@ -10,26 +10,26 @@ lennukiga(helsinki, paris, 180).
 lennukiga(paris, berlin, 120).
 lennukiga(paris, tallinn, 120).
 
-edge(Start, End) :-
-             lennukiga(Start, End, X);
-             rongiga(Start, End, X);
-             bussiga(Start, End, X);
-             laevaga(Start, End, X);
-             lennukiga(End, Start, X);
-             rongiga(End, Start, X);
-             bussiga(End, Start, X);
-             laevaga(End, Start, X).
+
+mineVahend(Start, End, lennukiga) :- lennukiga(Start, End, _); lennukiga(End, Start, _).
+mineVahend(Start, End, rongiga) :- rongiga(Start, End, _); rongiga(End, Start, _).
+mineVahend(Start, End, bussiga) :- bussiga(Start, End, _); bussiga(End, Start, _).
+mineVahend(Start, End, laevaga) :- laevaga(Start, End, _); laevaga(End, Start, _).
+
+
+mineHind(Start, End, Price) :-
+    lennukiga(Start, End, Price);
+    rongiga(Start, End, Price);
+    bussiga(Start, End, Price);
+    laevaga(Start, End, Price);
+    lennukiga(End, Start, Price);
+    rongiga(End, Start, Price);
+    bussiga(End, Start, Price);
+    laevaga(End, Start, Price).
 
 
 reisi(Start, End) :-
-    lennukiga(Start, End, X);
-    rongiga(Start, End, X);
-    bussiga(Start, End, X);
-    laevaga(Start, End, X);
-    lennukiga(End, Start, X);
-    rongiga(End, Start, X);
-    bussiga(End, Start, X);
-    laevaga(End, Start, X).
+    mineHind(Start, End, _).
 
 
 reisi(Start, End) :-
@@ -40,11 +40,59 @@ reisi(Start, End) :-
     reisi(Start, X),
     reisi(X, End).
 
-path(Node, Node, _, [Node]).
-path(Start, Finish, Visited, [Start | Path]) :-
-     edge(Start, X),
-     not(member(X, Visited)),
-     path(X, Finish, [X | Visited], Path).
+path(Node, Node, _, Node).
+path(Start, Finish, Visited, mine(Start, Next)) :-
+    mineHind(Start, Stop, _),
+    not(member(Stop, Visited)),
+    path(Stop, Finish, [Stop | Visited], Next).
 
 reisi(Start, End, Road) :-
     path(Start, End, [Start], Road).
+
+
+path2(Node, Node, _, Node).
+path2(Start, Finish, Visited, mine(Start, Stop, Transport, Next)) :-
+    mineVahend(Start, Stop, Transport),
+    not(member(Stop, Visited)),
+    path2(Stop, Finish, [Stop | Visited], Next).
+
+
+reisi_transpordiga(Start, End, Road) :-
+    path2(Start, End, [Start], Road).
+
+
+path3(Node, Node, _, Node, 0).
+path3(Start, Finish, Visited, mine(Start, Next), NewPrice) :-
+    mineHind(Start, Stop, Trip),
+    not(member(Stop, Visited)),
+    NewPrice is +(Price, Trip),
+    path3(Stop, Finish, [Stop | Visited], Next, Price).
+
+
+reisi(Start, End, Road, Price) :-
+    path3(Start, End, [Start], Road, Price).
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
