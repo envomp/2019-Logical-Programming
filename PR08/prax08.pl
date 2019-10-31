76dd63849db753447712fe1865aebb8f4b0eaf56
envomp@ttu.ee <envomp@ttu.ee>
2019-10-31 18:24:10 +0200

    PR08 typo

--- PR08/prax08.pl
+++ PR08/prax08.pl
@@ -10,13 +10,23 @@ is_a(zooplankton, mitte-roovloomad).
 is_a(veetaimed, mitte-roovloomad).
 is_a(vesikatk, veetaimed).
 is_a(vetikas, veetaimed).
+is_a(a1, a).
+is_a(a2, a).
+is_a(a3, a).
+is_a(a4, a).
+is_a(a5, a).
+is_a(a6, a).
+is_a(a7, a).
+is_a(a8, a).
+is_a(b1, b).
 
 eats(zooplankton, veetaimed).
 eats(kalad, zooplankton).
 eats(veeimetajad, kalad).
+eats(b, a).
 
 get_len([], 0).
-get_len([Head|Tail], Count) :- get_len(Tail, Sum), Count is Sum + 1.
+get_len([_|Tail], Count) :- get_len(Tail, Sum), Count is Sum + 1.
 
 terminal(Leaf, Leaf) :- not(is_a(_, Leaf)).
 terminal(Class, Leaf) :- is_a(SubClass, Class), terminal(SubClass, Leaf).
@@ -32,5 +42,5 @@ extinction(Pray, DeadSpieces, DeadCount) :- eats(Predator, Pray), extinction(Pre
 cycle :- asserta(best(bump, [], 0)), eats(_, Pray), extinction(Pray, TempSpieces, TempCount),
     ((best(_, _, Best), TempCount > Best, retractall(best(_, _, _)), asserta(best(Pray, TempSpieces, TempCount))); true).
 
-find_most_sensitive_species(Spieces, DeadSpieces, DeadCount) :- cycle, !, best(Spieces, DeadSpieces, DeadCount).
+find_most_sensitive_species(Spieces, DeadSpieces, DeadCount) :- findall(_, cycle, _), best(Spieces, DeadSpieces, DeadCount).
 
