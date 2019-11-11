dc930052f7d56197f7cf1321753949ac42a0c459
envomp@ttu.ee <envomp@ttu.ee>
2019-11-11 16:26:07 +0200

    Sentences should in fact end with a period. But who am i to judge.

--- PR09/prax09.pl
+++ PR09/prax09.pl
@@ -1,30 +1,44 @@
-liitlause --> (tuvi, sidesona, tuvi) ; (tuvi, sidesona, lihtlause).
-liitkusilause --> kusiaatom, lihtlisa, kusialgusliit, kusilisa, kusilopp.
+%liitlause --> (tuvi, sidesona, tuvi) ; (tuvi, sidesona, lihtlause).
+%liitkusilause --> kusiaatom, lihtlisa, kusialgusliit, kusilisa, kusilopp.
+%
+%lihtlisa --> tuvi ; (tuvi, sidesona, lihtlisa).
+%kusilisa --> tuvi ; (tuvi, sidesona, kusilisa).
+%
+%lihtlause --> lihtlisa, (lihtlopp ; []).
+%lihtkusilause --> kusialgus, tuvi, kusilopp.
+%
+%tuvi --> pretuvi, posttuvi.
+%pretuvi --> (((([] ; osastav), nimisonafraas), tegusonafraas) ; (tegusonafraas)).
+%posttuvi --> ([] ; nimisonafraas), ([] ; liitnimisona), ([] ; nimisonafraas).
+%
+%nimisonafraas --> ([] ; omadussonafraas), nimisona.
+%omadussonafraas --> ([] ; maarsona), omadussona.
+%tegusonafraas --> ([]; [ei]), tegusona , ([] ; nimisonafraas ; omadussonafraas).
 
-lihtlisa --> tuvi ; (tuvi, sidesona, lihtlisa).
-kusilisa --> tuvi ; (tuvi, sidesona, kusilisa).
 
-lihtlause --> lihtlisa, (lihtlopp ; []).
-lihtkusilause --> kusialgus, tuvi, kusilopp.
+liitlause --> lihtlause, uhend, lihtlause.
+liitlause --> liitlause, uhend, lihtlause.
+uhend --> [','].
+lihtlause --> nimisonafraas, tegusonafraas.
+nimisonafraas --> maarsonafraas, nimisona.
+maarsonafraas --> omadussona, maarsona ; [].
+maarsona --> [kivile] ; [upakile].
+nimisona --> [sammal] ; [uhkus] ; [raha] ; [volad].
+omadussona --> [veerevale] ; [].
+tegusona --> [kasva] ; [ajab] ; [tuleb] ; [laheb] ; [jaavad].
+tegusonafraas --> meetod, tegusona, maarsonafraas.
+meetod --> [ei] ; [].
 
-tuvi --> pretuvi, posttuvi.
-pretuvi --> (((([] ; osastav), nimisonafraas), tegusonafraas) ; (tegusonafraas)).
-posttuvi --> ([] ; nimisonafraas), ([] ; liitnimisona), ([] ; nimisonafraas).
-
-nimisonafraas --> ([] ; omadussonafraas), nimisona.
-omadussonafraas --> ([] ; maarsona), omadussona.
-tegusonafraas --> ([]; [ei]), tegusona , ([] ; nimisonafraas ; omadussonafraas).
-
-kusiaatom --> [kui].
-kusialgus --> [kas].
-kusialgusliit --> ["siis kas"].
-kusilopp --> ["?"].
-lihtlopp --> ["."] ; ["!"].
-liitnimisona --> [sobimatuse].
-omadussona --> [kole] ; [ilus] ; [lyhike] ; [must].
-maarsona --> [liiga].
-osastav --> [pakapiku] ; [tema].
-nimisona --> [inimene] ; [pakapiku] ; [tema] ; [habe] ; [jouluvanaks] ; [raha] ; [volad] ; [koer] ; [kass].
-tegusona --> [on] ; [kasvab] ; [elab] ; [tingib] ; [pohjustab] ; [tuleb] ; [laheb] ; [jaavad] ; [haugub] ; [nurrub].
-sidesona --> [ja] ; [voi] ; [",sest"] ; [","].
+%kusiaatom --> [kui].
+%kusialgus --> [kas].
+%kusialgusliit --> ["siis kas"].
+%kusilopp --> ["?"].
+%lihtlopp --> ["."] ; ["!"].
+%liitnimisona --> [sobimatuse].
+%omadussona --> [kole] ; [ilus] ; [lyhike] ; [must].
+%maarsona --> [liiga].
+%osastav --> [pakapiku] ; [tema].
+%nimisona --> [inimene] ; [pakapiku] ; [tema] ; [habe] ; [jouluvanaks] ; [raha] ; [volad] ; [koer] ; [kass].
+%tegusona --> [on] ; [kasvab] ; [elab] ; [tingib] ; [pohjustab] ; [tuleb] ; [laheb] ; [jaavad] ; [haugub] ; [nurrub].
+%sidesona --> [ja] ; [voi] ; [",sest"] ; [","].
 
