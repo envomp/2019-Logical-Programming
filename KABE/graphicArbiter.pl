3a80204fac2d519a1183b46d0a593c6e1081e231
envomp <envomp@taltech.ee>
2019-12-16 14:14:30 +0200

    kabe

--- KABE/graphicArbiter.pl
+++ KABE/graphicArbiter.pl
@@ -9,7 +9,7 @@
 %mustad(k1)     % mustadega mängib programm k1
 
 valged(iaib185787).		% Valged
-mustad(iaib185043).		% Mustad
+mustad(iaib185787).		% Mustad
 
 % Seejärel laadida mällu see programm (graphicArbiter) ja võistlevad kabeprogrammid.
 %                                                         _________________________
